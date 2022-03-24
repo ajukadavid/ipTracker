42408e0a4872f3c9a2c6d0395faadb462a75b5a4
Ajuka David <ajuka.zephiniah@gmail.com>
2022-03-24 16:55:21 +0100

    added component to vue IP address information

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
 <!--    Search /Results -->
-    <div class="flex justify-center relative px-4 pt-8 pb-32"></div>
+    <div class="flex justify-center bg-hero-pattern bg-cover relative px-4 pt-8 pb-32">
+
+      <!--  Search Input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+
+        <div class="flex">
+      <input type="text" class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+        placeholder="Search for any IP address or leave it empty to get your ip info"
+      />
+          <i class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fas fa-chevron-right"></i>
+        </div>
+      </div>
+      <IpInfo />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import IpInfo from '../components/IpInfo.vue'
 export default {
   name: 'HomeView',
+  components: {
+    IpInfo
+  }
 
 }
 </script>
